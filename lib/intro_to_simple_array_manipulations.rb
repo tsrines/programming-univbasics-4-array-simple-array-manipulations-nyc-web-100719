2a6baf6d84fc6cae877bb8765b1d5724271186c7
Timothy Rines <tsrines@gmail.com>
2019-09-18 18:10:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,3 +4,27 @@ end
 def using_unshift(array, string)
   array.unshift("Staten Island")
 end
+
+def using_pop
+  array.pop
+end
+
+def pop_with_args
+  array.pop(2)
+end
+
+def using_shift
+  array.shift
+end
+
+def shift_with_args
+  array.shift(2)
+end
+
+def using_concat
+  array.concat
+end
+
+def using_uniq
+  array.uniq
+end
